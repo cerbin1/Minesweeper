69c7f0c3d55ffb5f3d68084c1db9e16aab2b5a27
Bartek <bartek9393@poczta.onet.pl>
2016-12-14 17:24:05 +0100

    Created class GameFrame with title and defaultCloseOperation

---
+++ src/game/GameFrame.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+    public GameFrame() {
+        super("Minesweeper");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
