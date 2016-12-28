7d4546dc1ca540768250abd2965fc45787a568ea
Bartek <bartek9393@poczta.onet.pl>
2016-12-28 17:15:21 +0100

    Moved mouse listener from Application.java to Field.java

--- src/game/Field.java
+++ src/game/Field.java
@@ -2,6 +2,8 @@ package game;
 
 import javax.swing.JButton;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Field {
     static boolean isGameDone = false;
@@ -12,5 +14,34 @@ public class Field {
         button = new JButton();
         button.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
         isBomb = false;
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e.getButton());
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    if (Field.isGameDone) {
+                        System.out.println("Rozpocznij nowa gre");
+                    } else {
+                        if (isBomb) {
+                            System.out.println("Bomba, przegrales");
+                        } else {
+                            button.setText("x");
+                            System.out.println("Left mouse clicked");
+                        }
+                    }
+                }
+                if (Field.isGameDone) {
+                    System.out.println("Rozpocznij nowa gre");
+                } else {
+                    if (e.getButton() == MouseEvent.BUTTON3) {
+                        button.setText("?");
+                        System.out.println("Right mouse clicked");
+                    }
+                }
+            }
+        });
     }
 }
+
+
+
