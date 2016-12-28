99140eaedf4cbde5e80b3f25f1d899b36812e864
Bartek <bartek9393@poczta.onet.pl>
2016-12-28 16:22:38 +0100

    Added Field.java, with fields isBomb and button

---
+++ src/game/Field.java
@@ -0,0 +1,15 @@
+package game;
+
+import javax.swing.JButton;
+import java.awt.Dimension;
+
+public class Field {
+    boolean isBomb;
+    JButton button;
+
+    public Field(int buttonWidth, int buttonHeight) {
+        button = new JButton();
+        button.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
+        isBomb = false;
+    }
+}
