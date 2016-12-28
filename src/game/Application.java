a149a12d12541c8d86568cc297e210444a44e2ab
Bartek <bartek9393@poczta.onet.pl>
2016-12-28 01:07:33 +0100

    Implemented creating new frame, panel and buttons that imitate board

--- src/game/Application.java
+++ src/game/Application.java
@@ -1,9 +1,51 @@
 package game;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Application {
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Application extends JPanel {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Minesweeper");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                JButton button = new JButton("");
+                button.setPreferredSize(new Dimension(30, 30));
+
+                button.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        System.out.println(e.getButton());
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            System.out.println("Left mouse clicked");
+                        }
+
+                        if (e.getButton() == MouseEvent.BUTTON3) {
+                            System.out.println("Right mouse clicked");
+                        }
+                    }
+                });
+                panel.add(button);
+            }
+        }
+
+        frame.add(panel);
+    }
+
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> new GameFrame().displayGameBoard());
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
