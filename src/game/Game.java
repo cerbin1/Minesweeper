140ee177832eb563da545ab3693e17454431e314
Bartek <bartek9393@poczta.onet.pl>
2016-12-30 18:10:49 +0100

    change name of iterators

--- src/game/Game.java
+++ src/game/Game.java
@@ -23,9 +23,9 @@ public class Game {
     }
 
     void displayAllBombs(Field[][] fields) {
-        for (int k = 0; k < 10; k++) {
-            for (int l = 0; l < 10; l++) {
-                Field cell = fields[k][l];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Field cell = fields[i][j];
                 if (cell.isBomb) {
                     cell.button.setText("x");
                     cell.button.setForeground(Color.DARK_GRAY);
