8f0330b68ed3009d6de625ea71fc9e42e8f73a07
Bartek <bartek9393@poczta.onet.pl>
2017-02-16 00:43:56 +0100

    clean

--- src/game/Application.java
+++ src/game/Application.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 import static java.awt.Color.*;
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Application {
     private final static Color[] BOMBS_COUNTER_COLORS = {
